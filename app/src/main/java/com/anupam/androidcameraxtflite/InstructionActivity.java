59612eb090f5af3773f37e3a098515a87697ec73
satwikabhattacharjee <satwikab112@gmail.com>
2020-07-31 14:09:21 +0530

    added HomeActivity and InstructionActivity

--- app/src/main/java/com/anupam/androidcameraxtflite/InstructionActivity.java
+++ app/src/main/java/com/anupam/androidcameraxtflite/InstructionActivity.java
@@ -6,33 +6,22 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
-import android.view.View;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.concurrent.TimeUnit;
-
 import java.util.Locale;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeUnit;
+public class InstructionActivity extends AppCompatActivity {
 
-import static android.speech.tts.TextToSpeech.QUEUE_ADD;
-
-public class HomeActivity extends AppCompatActivity {
-
-    LinearLayout l1;
     TextToSpeech textToSpeech;
-    TextView welcomeId;
+    TextView instructionId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.activity_home);
-        welcomeId = findViewById(R.id.welcome);
-
-        welcomeId.setVisibility(View.VISIBLE);
-
+        setContentView(R.layout.activity_instruction);
+        instructionId = findViewById(R.id.instText);
         textToSpeech = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
             @Override
             public void onInit(int status) {
@@ -47,49 +36,30 @@ public class HomeActivity extends AppCompatActivity {
                         textToSpeech.setPitch(0.6f);
                         textToSpeech.setSpeechRate(1.0f);
 
-                            speak();
-
-                        //textToSpeech.playSilentUtterance(2000,QUEUE_ADD, null);
+                        speak();
                     }
                 }
             }
         });
-
-//        l1 = findViewById(R.id.welcomeScreen);
-//        l1.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//
-//            }
-//        });
-//            openInstructions();
-
-//
-
     }
 
-    private void openInstructions() {
+    private void openCamera() {
         try{
             TimeUnit.SECONDS.sleep(4);
-            startActivity(new Intent(getApplicationContext(),InstructionActivity.class));
+            startActivity(new Intent(getApplicationContext(),MainActivity.class));
         }catch(InterruptedException e){
             //
         }
     }
 
-    private void speak() {//throws InterruptedException {
-        String text = welcomeId.getText().toString();
+    private void speak() {
+        String text = instructionId.getText().toString();
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null, null);
         } else {
             textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null);
         }
-//        TimeUnit.SECONDS.sleep(3);
-//        for(int i = 0;i <= 5000;i++){
-//
-//        }
-        openInstructions();
+        openCamera();
     }
-
 
 }
