9c948f67b03f14c0f1b4b8aefdcd04cae1d182da
HemanthReddy69 <hemanthsai392@gmail.com>
2020-07-31 12:39:05 +0530

    Initial commit

---
+++ app/src/main/java/com/anupam/androidcameraxtflite/HomeActivity.java
@@ -0,0 +1,14 @@
+package com.anupam.androidcameraxtflite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
